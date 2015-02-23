ef77b099e52c8b256564371044ceeba73638e991
Yegor Tokmakov <yegor@tokmakov.biz>
2015-02-24 00:04:06 +0100

    layout update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # itf_platform
+
+Based on http://typesafe.com/activator/template/play-oauth2-scala
--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -7,15 +7,22 @@ import play.api.mvc._
 import util.OAuth2
 
 object Application extends Controller {
+  val FLASH_MESSAGE_KEY: String = "message"
+  val FLASH_ERROR_KEY: String = "error"
 
-  def index = Action { implicit request =>
-    val oauth2 = new OAuth2(Play.current)
-    val callbackUrl = util.routes.OAuth2.callback(None, None).absoluteURL()
-    val scope = "repo"   // github scope - request repo access
-    val state = UUID.randomUUID().toString  // random confirmation string
-    val redirectUrl = oauth2.getAuthorizationUrl(callbackUrl, scope, state)
-    Ok(views.html.index("Your new application is ready.", redirectUrl)).
-      withSession("oauth-state" -> state)
+  def index() = Action {
+    implicit request =>
+      val oauth2 = new OAuth2(Play.current)
+      val callbackUrl = util.routes.OAuth2.callback(None, None).absoluteURL()
+      val scope = "repo"   // github scope - request repo access
+      val state = UUID.randomUUID().toString  // random confirmation string
+      val redirectUrl = oauth2.getAuthorizationUrl(callbackUrl, scope, state)
+      Ok(views.html.index("Your new application is ready.", redirectUrl)).
+        withSession("oauth-state" -> state)
+  }
+
+  def blog = Action { implicit request =>
+    Ok(views.html.index("Your new application is ready.", "asd"))
   }
 
 }
